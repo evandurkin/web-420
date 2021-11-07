09c92b6f13038ab92f90351cfbdb77ff86572951
Evan Durkin <edurkin@my365.bellevue.edu>
2021-11-06 20:25:10 -0400

    adding week 3 files

---
+++ app.js
@@ -0,0 +1,39 @@
+// Title: Assignment 1.2
+// Author: Evan Durkin
+// Date: October 23, 2021
+
+
+// require statements
+const express = require("express");
+const http = require("http");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const mongoose = require("mongoose");
+
+
+// assigns express to a variable
+const app = express();
+
+// set statements
+app.set("port", process.env.PORT || 8080);
+
+// use statements
+app.use(express.json());
+app.use(express.urlencoded({"extended":true}));
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "WEB 420 RESTful APIs",
+            version: "1.0.0",
+        },
+    },
+    apis: ["./routes/*.js"], // files containing annotations for the OpenAPI specification
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+http.createServer(app).listen(app.get("port"), function()
+    { console.log("Application started and listening on port 8080")});
